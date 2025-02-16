85e14716d723ead3cf6bfb2eb2a1f62afd718c30
LIUCBK <liucbk@bc.edu>
2025-02-16 11:02:10 -0500

    Initial Commit

---
+++ wordGarden/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  wordGarden
+//
+//  Created by 刘佳雨 on 2025/2/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ wordGarden/wordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordGardenApp.swift
+//  wordGarden
+//
+//  Created by 刘佳雨 on 2025/2/16.
+//
+
+import SwiftUI
+
+@main
+struct wordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
